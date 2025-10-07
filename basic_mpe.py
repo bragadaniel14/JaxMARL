e4dead68c35c3aeee439f867609b13feea05d1ea
Daniel Lima Braga <dbraga@della-gpu.princeton.edu>
2025-10-06 21:05:22 -0400

    Small adjustments

--- basic_mpe.py
+++ basic_mpe.py
@@ -12,16 +12,20 @@ if __name__ == "__main__":
     print(f"We are dealing with device {jax.devices()}")
     algo_and_config_map = {
         "ippo_ff_mpe": (ippo_ff_mpe, OmegaConf.load(resources.files("baselines.Custom.config") / "ippo_ff_mpe_simple_spread.yaml")),
-        "ippo_rnn_mpe": (ippo_rnn_mpe, OmegaConf.load(resources.files("baselines.IPPO.config") / "ippo_rnn_mpe.yaml")),
-        "mappo_rnn_mpe": (mappo_rnn_mpe, OmegaConf.load(resources.files("baselines.MAPPO.config") / "mappo_homogenous_rnn_mpe.yaml"))
+        "ippo_rnn_mpe": (ippo_rnn_mpe, OmegaConf.load(resources.files("baselines.Custom.config") / "ippo_rnn_mpe_simple_spread.yaml")),
+        "mappo_rnn_mpe": (mappo_rnn_mpe, OmegaConf.load(resources.files("baselines.Custom.config") / "mappo_homogenous_rnn_mpe_simple_spread.yaml"))
     }
-
-    parser = argparse.ArgumentParser(description="A simple starter script")
+    
+    parser = argparse.ArgumentParser()
     parser.add_argument("--algo_type", type=str, default="ippo_ff_mpe")
+    parser.add_argument("--script_name", type=str, default=None)
     args = parser.parse_args()
-
     algo, config = algo_and_config_map[args.algo_type]
+    
+    if args.script_name:
+        config = OmegaConf.load(resources.files("baselines.Custom.config") / args.script_name)
 
+    print(f"Running MPE Training with args {args}, and config {config}")
     algo.main(config)
 
-    print(f"Finished rujnning algo {args.algo_type}")
+    print(f"Finished running algo {args.algo_type}")
